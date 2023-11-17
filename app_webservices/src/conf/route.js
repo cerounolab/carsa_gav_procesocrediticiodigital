c1a34f5a3ab2d5b2c9ceb95cb57b1d382ec08dd9
CEROUNO02 - Marial del Carmen Gaona Santacruz <mgaona@cerouno.com.py>
2023-11-17 12:43:09 -0300

    CARDEVPCD-12_1

--- app_webservices/src/conf/route.js
+++ app_webservices/src/conf/route.js
@@ -7,7 +7,9 @@ require('express-group-routes');
 const { getDominio, getDominioId, getDominioValor, postDominio, putDominio, deleteDominio} = require('../controllers/DOMFICController');
 const { getEmpresa, getEmpresaId, getEmpresaRUC, getEmpresaTipoRubro, postEmpresa, putEmpresa, deleteEmpresa } = require('../controllers/EMPFICController');
 const { getSucursal, getSucursalId, getSucursalEmpresaId, getEmpresaTipoSucursal, postSucursal, putSucursal, deleteSucursal } = require('../controllers/SUCFICController');
-const { getUsuario, getUsuarioId, getUsuarioEmpresaId, getUsuarioSucursalId, getUsuarioDocumento, getUsuarioUsu, postUsuario,  postUsuarioLogin, putUsuario, deleteUsuario} = require('../controllers/USUFICController');
+const { getUsuario, getUsuarioId, getUsuarioEmpresaId, getUsuarioSucursalId, getUsuarioDocumento, getUsuarioUsu, getUsuarioDocumentoEmpresa, postUsuario,  postUsuarioLogin, putUsuario, deleteUsuario} = require('../controllers/USUFICController');
+
+
 
 router.group('/v1/', (routerGroup) => {
   routerGroup.get('/dominio/listado', getDominio);
@@ -38,13 +40,13 @@ router.group('/v1/', (routerGroup) => {
   routerGroup.get('/usuario/empresa/:empresa', getUsuarioEmpresaId);
   routerGroup.get('/usuario/sucursal/:sucursal', getUsuarioSucursalId);
   routerGroup.get('/usuario/documento/:documento', getUsuarioDocumento);
+  routerGroup.get('/usuario/documento/:documento/empresa/:empresa', getUsuarioDocumentoEmpresa);
   routerGroup.get('/usuario/usuario/:usuario', getUsuarioUsu);
   routerGroup.post('/usuario/registro', postUsuario);
   routerGroup.post('/usuario/login', postUsuarioLogin);
   routerGroup.put('/usuario/:codigo', putUsuario);
   routerGroup.delete('/usuario/:codigo', deleteUsuario);
 
-  
 }); 
 
 module.exports = router;
