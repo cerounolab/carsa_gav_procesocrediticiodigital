6327154d7d021a50ba46aebc812c075545a8e2e3
CEROUNO02 - Marial del Carmen Gaona Santacruz <mgaona@cerouno.com.py>
2023-11-27 12:48:01 -0300

    CARDEVPCD-31

--- app_webservices/src/helpers/sql_delete.js
+++ app_webservices/src/helpers/sql_delete.js
@@ -4,6 +4,7 @@ const {errorBody}   = require('../utils/_json');
 /**
     * @param {integer} codigo - codigo
     * @param {integer} codigo2 - codigo2
+    * @param {integer} codigo3 - codigo3
     * @returns {Array} returns
 */
 
@@ -347,6 +348,47 @@ const deleteROLFOR  = async(codigo, codigo2) => {
     return Array(_code, _data);
 }
 
+const deleteUSUROL  = async(codigo, codigo2) => {
+
+    let _code   = 200;
+    let _data   = [];
+   
+    let query00 = '';
+
+    query00 = `DELETE FROM adm.USUROL WHERE USUROLUSC =  ${codigo} AND USUROLROC = ${codigo2}`;	
+    const connPGSQL = new Client(initPGSQL);
+    
+    await connPGSQL
+        .connect()
+        .catch(e => {
+            _code = 401;
+            errorBody(_code, 'Code: '+ e.code + ', Routine: ' + e.routine + ', Function: deleteUSUROL', true)
+                .then(result => _data = result);
+        }
+    );
+
+    if (_code == 200) {
+        await connPGSQL
+            .query(query00)
+            .then(result => {
+                _code = 200;
+                _data = result.rows;
+            })
+            .catch(e => {
+                _code = 500;
+                console.log(e);
+                errorBody(_code, 'Code: '+ e.code + ', Routine: ' + e.routine + ', Function: deleteUSUROL', true)
+                    .then(result => _data = result);
+            })
+            .then(() => {
+                connPGSQL.end();
+            }
+        );
+    }
+    
+    return Array(_code, _data);
+}
+
 module.exports = {
     deleteDOMFIC,
     deleteEMPFIC,
@@ -355,5 +397,6 @@ module.exports = {
     deleteROLFIC,
     deleteCAMFIC,
     deleteFORFIC, 
-    deleteROLFOR
+    deleteROLFOR,
+    deleteUSUROL
 };
